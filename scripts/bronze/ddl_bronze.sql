f7a829a98d10af0b3e3e1cbf638bbe8aa1fe65bf
Victor <victorsrp23@gmail.com>
2025-10-17 13:51:54 -0300

    Update & rename ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,90 @@
+/*
+================================================================================
+DDL Script:     Create Bronze Tables
+Author:         Victor Pereira
+Create date:    [Data de Criação, ex: 2025-10-17]
+Version:        1.0
+================================================================================
+Script Purpose:
+	Este script tem o propósito de criar as tabelas no Schema Bronze para
+	receber os dados brutos das origens CRM e ERP.
+
+Execution Logic:
+	O script irá verificar a existência de tabelas com o mesmo nome,
+	excluí-las (DROP IF EXISTS) e então recriá-las com a estrutura definida.
+================================================================================
+*/
+USE DataWarehouse;
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info(
+	cst_id			   INT,
+	cst_key			   NVARCHAR(50),
+	cst_firstname	   NVARCHAR(50),
+	cst_lastname	   NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr           NVARCHAR(50),
+	cst_create_date    DATE
+);
+GO
+
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info(
+	prd_id		 INT,
+	prd_key		 NVARCHAR(50),
+	prd_nm		 NVARCHAR(50),
+	prd_cost	 INT,
+	prd_line	 NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt   DATETIME
+);
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num  NVARCHAR(50),
+	sls_prd_key  NVARCHAR(50),
+	sls_cust_id  INT,
+	sls_order_dt INT,
+	sls_ship_dt  INT,
+	sls_due_dt   INT,
+	sls_sales    INT,
+	sls_quantity INT,
+	sls_price    INT
+);
+GO
+
+IF OBJECT_ID('bronze.erp_custaz12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_custaz12;
+
+CREATE TABLE bronze.erp_custaz12(
+	CID   NVARCHAR(50),
+	BDATE DATE,
+	GEN   NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_loca101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loca101;
+
+CREATE TABLE bronze.erp_loca101(
+	CID	  NVARCHAR(50),
+	CNTRY NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_catg1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_catg1v2;
+
+CREATE TABLE bronze.erp_px_catg1v2(
+ID			NVARCHAR(50),
+CAT			NVARCHAR(50),
+SUBCAT		NVARCHAR(50),
+MAINTENANCE NVARCHAR(50)
+);
--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,11 +1,42 @@
---USE master;
+/*
+=============================================================
+Criação do Banco de Dados e Schemas
+=============================================================
+Propósito do Script:
+    Este script cria um novo banco de dados chamado 'DataWarehouse' após verificar se ele já existe. 
+    Se o banco de dados existir, ele será removido e recriado. Além disso, o script configura três schemas 
+    dentro do banco de dados: 'bronze', 'silver' e 'gold'.
+	
+ATENÇÃO:
+    A execução deste script removerá completamente o banco de dados 'DataWarehouse' caso ele já exista. 
+    Todos os dados contidos no banco de dados serão permanentemente excluídos. Prossiga com cautela 
+    e certifique-se de ter backups adequados antes de executar este script.
+*/
 
---CREATE DATABASE DataWarehouse;
+USE master;
+GO
 
---USE DataWarehouse;
+-- Deletendo e recriando 'DataWarehouse' database caso ja exista
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
+-- Criando o 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Criando Schemas
 CREATE SCHEMA bronze;
 GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
+GO
